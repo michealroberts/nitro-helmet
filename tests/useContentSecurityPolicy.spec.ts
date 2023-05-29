0be57533bdff2565cceeb7fbb7a0e4dbb983cff1
Michael J. Roberts <michael@observerly.com>
2023-05-29 11:02:28 +0100

    chore: Amended code header comment.
    chore: Amended code header comment.

--- tests/useContentSecurityPolicy.spec.ts
+++ tests/useContentSecurityPolicy.spec.ts
@@ -1,7 +1,7 @@
 /*****************************************************************************************************************/
 
 // @author         Michael Roberts <michael@observerly.com>
-// @package        @observerly/nitro-cors
+// @package        @observerly/nitro-helmet
 // @license        Copyright © 2021-2023 observerly
 
 /*****************************************************************************************************************/
